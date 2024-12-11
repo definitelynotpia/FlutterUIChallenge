5686f136b92c8235a450df70dbe715dc0fde93b9
definitelynotpia <frances.rey@ciit.edu.ph>
2024-12-11 12:47:17 +0800

    food delivery and order details pages initialized

---
+++ lib/screens/food_delivery.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FoodDelivery extends StatefulWidget {
+  const FoodDelivery({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _FoodDelivery();
+}
+
+class _FoodDelivery extends State<FoodDelivery> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screens/oder_details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OrderDetails extends StatefulWidget {
+  const OrderDetails({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _OrderDetails();
+}
+
+class _OrderDetails extends State<OrderDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
